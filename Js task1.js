a89897d2f98e75b9d412b46273843d408642df60
abraham1st <74826424+abraham1st@users.noreply.github.com>
2022-06-22 14:23:43 +0100

    Add files via upload

---
+++ Js task1.js
@@ -0,0 +1,7 @@
+// Javascript Task1
+var name = "Abraham Lawal";
+var height = "6.0ft";
+var country = "Nigeria";
+console.log("Name: " + name);
+console.log("Height: " + height);
+console.log("Country: " + country);
